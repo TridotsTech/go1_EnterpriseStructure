6bbd3d00171832dbbaf9a61b248b51581a777fa8
Tridots Tech <info@tridotstech.com>
2024-09-24 12:24:10 +0000

    fix:fixed the url for the api

---
+++ go1_enterprise_structure/go1_enterprise_structure/api.py
@@ -0,0 +1,37 @@
+import frappe
+
+@frappe.whitelist()
+def get_organisation_levels():
+	levels = frappe.db.get_all("Organisation Level", pluck='name', order_by='organisation_level asc')
+	if levels:
+		levels = "\n".join(levels)
+	frappe.response.message = levels
+
+@frappe.whitelist()
+def get_organisation_parents(organisation_type=None):
+	level = frappe.db.get_value("Organisation Level", organisation_type, 'organisation_level')
+	if not level:
+		level = 0
+	else:
+		level = level - 1
+	organisation_parent_type = frappe.db.get_value("Organisation Level", pluck='name', filters={"organisation_level":level})
+	organisation_parent_names = frappe.db.get_all("Organisation Structure", filters={"organisation_type":organisation_parent_type}, pluck='name')
+	frappe.response.message = organisation_parent_names
+
+@frappe.whitelist()
+def get_worksite_levels():
+	levels = frappe.db.get_all("Worksite Level", pluck='name', order_by='worksite_level asc')
+	if levels:
+		levels = "\n".join(levels)
+	frappe.response.message = levels
+
+@frappe.whitelist()
+def get_worksite_parents(worksite_type=None):
+	level = frappe.db.get_value("Worksite Level", worksite_type, 'worksite_level')
+	if not level:
+		level = 0
+	else:
+		level = level - 1
+	worksite_parent_type = frappe.db.get_value("Worksite Level", pluck='name', filters={"worksite_level":level})
+	worksite_parent_names = frappe.db.get_all("Worksite Structure", filters={"worksite_type":worksite_parent_type}, pluck='name')
+	frappe.response.message = worksite_parent_names
