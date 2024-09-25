4754e4be0036682a7f530dbaa25c61415e89b23d
Tridots Tech <info@tridotstech.com>
2024-09-25 10:04:12 +0000

    fix: Fixed the issue with level 0 filters and updated the level doctypes as required.

--- go1_enterprise_structure/go1_enterprise_structure/api.py
+++ go1_enterprise_structure/go1_enterprise_structure/api.py
@@ -14,9 +14,12 @@ def get_organisation_parents(organisation_type=None):
 		level = 0
 	else:
 		level = level - 1
-	organisation_parent_type = frappe.db.get_value("Organisation Level", pluck='name', filters={"organisation_level":level})
-	organisation_parent_names = frappe.db.get_all("Organisation Structure", filters={"organisation_type":organisation_parent_type}, pluck='name')
-	frappe.response.message = organisation_parent_names
+	if level != 0:
+		organisation_parent_type = frappe.db.get_value("Organisation Level", pluck='name', filters={"organisation_level":level})
+		organisation_parent_names = frappe.db.get_all("Organisation Structure", filters={"organisation_type":organisation_parent_type}, pluck='name')
+		frappe.response.message = organisation_parent_names
+	else:
+		frappe.response.message = None
 
 @frappe.whitelist()
 def get_worksite_levels():
@@ -32,6 +35,9 @@ def get_worksite_parents(worksite_type=None):
 		level = 0
 	else:
 		level = level - 1
-	worksite_parent_type = frappe.db.get_value("Worksite Level", pluck='name', filters={"worksite_level":level})
-	worksite_parent_names = frappe.db.get_all("Worksite Structure", filters={"worksite_type":worksite_parent_type}, pluck='name')
-	frappe.response.message = worksite_parent_names
+	if level != 0:
+		worksite_parent_type = frappe.db.get_value("Worksite Level", pluck='name', filters={"worksite_level":level})
+		worksite_parent_names = frappe.db.get_all("Worksite Structure", filters={"worksite_type":worksite_parent_type}, pluck='name')
+		frappe.response.message = worksite_parent_names
+	else:
+		frappe.response.message = None
